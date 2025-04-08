5bf0fe7e76e9cdd62b8dcf8a471ddb8ab34c39f7
Jackson Lloyd <jackson.lloyd@pointsbet.com>
2025-04-08 23:08:58 +0100

    Add initial dapr test

--- setup/scripts/local-mini-start.sh
+++ setup/scripts/local-mini-start.sh
@@ -1,6 +1,10 @@
 # Start minikube using docker
 minikube start --driver=docker --memory 2G
 
+minikube addons enable metrics-server
+minikube addons enable dashboard
+minikube addons enable ingress
+
 # Create a k8s namespace for ArgoCD
 argo_namespace="argocd"
 kubectl create ns $argo_namespace
@@ -17,7 +21,7 @@ kubectl get all -n $argo_namespace
 # Print ArgoCD UI admin's password
 kubectl -n $argo_namespace get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
-kubectrl apply -n $argo_namespace -f ../configure-repo.yaml
+kubectl apply -n $argo_namespace -f ../configure-repo.yaml
 
 # Port forward ArgoCD HTTP UI's port onto local machine's 8030 port
 # Access via the browser using the above password with 'admin' username
